af34e21607786436238fc1231e0a2fc77b0b42f8
Jayden <apha3986@uni.sydney.edu.au>
2024-11-26 19:13:21 +1100

    header v1

--- script.js
+++
@@ -1,8 +0,0 @@
-// Toggle dark mode and light mode
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleButton = document.getElementById("darkModeToggle");
-
-    toggleButton.addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    });
-});
